546780a26cc07578a372920b13fd6a3ecb97413a
MdNoor300 <sehzad.noor98@gmail.com>
2024-05-04 11:59:11 +0600

    pusher server add done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/chat', [ChatController::class, 'index']);
+Route::post('/send-message', [ChatController::class, 'sendMessage']);
